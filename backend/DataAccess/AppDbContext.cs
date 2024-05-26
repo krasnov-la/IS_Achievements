e7febbb9375421c1e676c3e53e3d8e651c6d3967
Krasnov-la <Leonid.Krasnov.R255@gmail.com>
2024-05-26 20:17:30 +1000

    removed unused method

--- backend/DataAccess/AppDbContext.cs
+++ backend/DataAccess/AppDbContext.cs
@@ -5,9 +5,4 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-
-    }
 }
