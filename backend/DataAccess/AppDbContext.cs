1d10cac2d02b342d6319270e13d4e6346679a2bc
Albert Zagibin <albert.zagibin@gmail.com>
2024-05-26 18:36:16 +1000

    add db to services

---
+++ backend/DataAccess/AppDbContext.cs
@@ -0,0 +1,13 @@
+namespace DataAccess;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,13 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
