a20d51c99ad54029c90eaaf4075892da66a07da0
topi768 <90519641+topi768@users.noreply.github.com>
2024-05-27 22:47:08 +1000

    sidebar

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <side-bar />
+  <router-view/>
+
+  </div>
+</template>
+
+<script>
+import SideBar from "./components/SideBar";
+export default {
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+html, body {
+  height: 100%;
+  margin: 0;
+}
+* {
+  box-sizing: border-box;
+}
+</style>
