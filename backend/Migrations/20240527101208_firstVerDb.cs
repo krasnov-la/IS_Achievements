0e39979482ab89b6adf62240268ab848a236a277
Albert Zagibin <albert.zagibin@gmail.com>
2024-05-27 20:38:50 +1000

    add migrations

---
+++ backend/Migrations/20240527101208_firstVerDb.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstVerDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Login = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Nickname = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Role = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Password = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Login);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Datetime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Link = table.Column<string>(type: "character varying(2100)", maxLength: 2100, nullable: false),
+                    AdminLogin = table.Column<string>(type: "character varying(256)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activities_Users_AdminLogin",
+                        column: x => x.AdminLogin,
+                        principalTable: "Users",
+                        principalColumn: "Login",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VerificationRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerLogin = table.Column<string>(type: "character varying(256)", nullable: false),
+                    EventName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsOpen = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VerificationRequests_Users_OwnerLogin",
+                        column: x => x.OwnerLogin,
+                        principalTable: "Users",
+                        principalColumn: "Login",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Achievements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Score = table.Column<float>(type: "real", nullable: false),
+                    VerificationDatetime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AdminLogin = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Achievements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Achievements_Users_AdminLogin",
+                        column: x => x.AdminLogin,
+                        principalTable: "Users",
+                        principalColumn: "Login",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Achievements_VerificationRequests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "VerificationRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Datetime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Text = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_VerificationRequests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "VerificationRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequestId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Images_VerificationRequests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "VerificationRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achievements_AdminLogin",
+                table: "Achievements",
+                column: "AdminLogin");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Achievements_RequestId",
+                table: "Achievements",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_AdminLogin",
+                table: "Activities",
+                column: "AdminLogin");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_RequestId",
+                table: "Comments",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_RequestId",
+                table: "Images",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationRequests_OwnerLogin",
+                table: "VerificationRequests",
+                column: "OwnerLogin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Achievements");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "VerificationRequests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
