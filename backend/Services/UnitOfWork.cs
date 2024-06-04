96990bb1bb38925904da1bb9bd4424055378d896
Krasnov-la <Leonid.Krasnov.R255@gmail.com>
2024-06-04 17:24:32 +1000

    realisation

---
+++ backend/Services/UnitOfWork.cs
@@ -0,0 +1,36 @@
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services;
+
+public class UnitOfWork : IUnitOfWork
+{
+    DbContext _db;
+
+    public UnitOfWork(DbContext db)
+    {
+        _db = db;
+        Achievements = new GenericRepository<Achievement>(_db);
+        Activities = new GenericRepository<Activity>(_db);
+        Comments = new GenericRepository<Comment>(_db);
+        Images = new GenericRepository<Image>(_db);
+        Users = new GenericRepository<User>(_db);
+        Requests = new GenericRepository<VerificationRequest>(_db);
+    }
+    public IRepository<Achievement> Achievements {get; private set;}
+
+    public IRepository<Activity> Activities {get; private set;}
+
+    public IRepository<Comment> Comments {get; private set;}
+
+    public IRepository<Image> Images {get; private set;}
+
+    public IRepository<User> Users {get; private set;}
+
+    public IRepository<VerificationRequest> Requests {get; private set;}
+
+    public async Task SaveAsynch()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
