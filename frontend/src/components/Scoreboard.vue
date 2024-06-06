c773d0c275ff136c8624ae4064c77e19154d8410
Albert Zagibin <albert.zagibin@gmail.com>
2024-06-06 22:59:15 +1000

    add prittier for vs code and refactoring pages

--- frontend/src/components/Scoreboard.vue
+++ frontend/src/components/Scoreboard.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="widget_item scoreboard">
-    <div class="scoreboard-header" >
-  <h2>Scoreboard</h2>
-    
-    <img class="sort" @click="sortPlayers" src="../assets/Group 2.png" alt="">
+    <div class="scoreboard-header">
+      <h2>Scoreboard</h2>
+
+      <img
+        class="sort"
+        @click="sortPlayers"
+        src="../assets/Group 2.png"
+        alt=""
+      />
     </div>
 
     <div class="table">
@@ -20,62 +25,58 @@
     </div>
   </div>
 </template>
-<script setup >
-import { ref, createApp, } from 'vue'
-const sortIndex = ref(0)
-    const players = ref([
-      { name: 'John', score: 100245, id: 1234 },
-      { name: 'Jane', score: 93998, id: 1235 },
-      { name: 'Bob', score: 86277, id: 1236 },
-      { name: 'John', score: 100245, id: 1234 },
-      { name: 'Jane', score: 93998, id: 1235 },
-      { name: 'Bob', score: 86277, id: 1236 },
-    ])
+<script setup>
+import { ref, createApp } from "vue";
+const sortIndex = ref(0);
+const players = ref([
+  { name: "John", score: 100245, id: 1234 },
+  { name: "Jane", score: 93998, id: 1235 },
+  { name: "Bob", score: 86277, id: 1236 },
+  { name: "John", score: 100245, id: 1234 },
+  { name: "Jane", score: 93998, id: 1235 },
+  { name: "Bob", score: 86277, id: 1236 },
+]);
 
 function sortPlayers() {
   sortIndex.value++; // Переход к следующему полю для сортировки
-  let fields = ['name', 'score']; // Массив полей для сортировки
+  let fields = ["name", "score"]; // Массив полей для сортировки
   if (sortIndex.value >= fields.length) {
     sortIndex.value = 0; // Сброс индекса, если все поля были пройдены
   }
   players.value.sort((a, b) => {
     switch (fields[sortIndex.value]) {
-      case 'name':
-        return String(a[fields[sortIndex.value]]).localeCompare(String(b[fields[sortIndex.value]]));
-      case 'score':
+      case "name":
+        return String(a[fields[sortIndex.value]]).localeCompare(
+          String(b[fields[sortIndex.value]])
+        );
+      case "score":
         return a[fields[sortIndex.value]] - b[fields[sortIndex.value]];
       default:
         return 0;
     }
   });
-const sortedPlayers = computed(() => players.value.slice()); // Копируем массив для отображения
-
-
+  const sortedPlayers = computed(() => players.value.slice()); // Копируем массив для отображения
 }
-const app = createApp({})
+const app = createApp({});
 
 // Регистрация компонента
 </script>
 <style scoped>
 h2 {
-    /* Scoreboard */
+  /* Scoreboard */
 
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 36px;
 
-
-font-family: 'Inter';
-font-style: normal;
-font-weight: 600;
-font-size: 30px;
-line-height: 36px;
-
-color: #E3E4E4;
-
-
+  color: #e3e4e4;
 }
 .widget_item {
-background: #232627;
-    background: #35373A;
-    border-radius: 20px;
+  background: #232627;
+  background: #35373a;
+  border-radius: 20px;
 }
 .scoreboard-header {
   display: flex;
@@ -92,48 +93,48 @@ background: #232627;
 }
 
 .table-header {
-position: relative;
+  position: relative;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 500;
-font-size: 15px;
-line-height: 18px;
-    background: #343839;
-color: #FFFFFF;
-min-height: 50px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 18px;
+  background: #343839;
+  color: #ffffff;
+  min-height: 50px;
 }
 .table-item {
-    /* doppio23s */
-    width: 100%;
-    /* justify-content: space-around;
+  /* doppio23s */
+  width: 100%;
+  /* justify-content: space-around;
 display: flex; */
-position: relative;
-font-family: 'Inter';
-font-style: normal;
-font-weight: 300;
-font-size: 15px;
-line-height: 18px;
+  position: relative;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 18px;
 
-color: #FFFFFF;
-min-height: 50px;
+  color: #ffffff;
+  min-height: 50px;
 }
 /* .table-item:nth-child(2n) {
     background: #35373A;
 } */
-.table-item:nth-child(2n+1) {
-    background: #343839;
+.table-item:nth-child(2n + 1) {
+  background: #343839;
 }
 .number {
-    position: absolute;
-    left: 5%;
+  position: absolute;
+  left: 5%;
 }
 .name {
-    position: absolute;
-    left: 45%;
+  position: absolute;
+  left: 45%;
 }
-.score { 
-    position: absolute;
-    left: 75%;
+.score {
+  position: absolute;
+  left: 75%;
 }
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,127 +1,104 @@
 <template>
-<div class="wrapper">
-   <side-bar />
-    <div class="main" >
-        
-        <header>Главная страница</header>
-        <div class="widgets" >
-            <scoreboard  />
-            <events-list />
-            <future-events-list />
-
-        </div>
+  <div class="wrapper">
+    <side-bar />
+    <div class="main">
+      <header>Главная страница</header>
+      <div class="widgets">
+        <scoreboard />
+        <events-list />
+        <future-events-list />
+      </div>
     </div>
-</div>
- 
-
-
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
-<script>
-import { ref, createApp } from 'vue'
-import SideBar from '../components/SideBar.vue'
-import EventsList from '../components/EventsList.vue'
-import FutureEventsList from '../components/FutureEventsList.vue'
-
-import Scoreboard from '../components/Scoreboard.vue'
-
-export default {
-  components: { SideBar, EventsList, Scoreboard, FutureEventsList },
-
-}
-const app = createApp({})
-
-// Регистрация компонента
-app.component('side-bar', SideBar)
+<script setup>
+import SideBar from "../components/SideBar.vue";
+import EventsList from "../components/EventsList.vue";
+import FutureEventsList from "../components/FutureEventsList.vue";
+import Scoreboard from "../components/Scoreboard.vue";
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap");
 
 .wrapper {
-    height: 100%;
-    display: flex;
+  height: 100%;
+  display: flex;
 }
 .main {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    background: #1c1e1f;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: #1c1e1f;
 }
 header {
-    /* position: absolute; */
-    width: 100%;
-    background: #232627;
-    /* height: 50px; */
-    padding: 10px 10px;
-    border: 1px solid #35373A;
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 33px;
-line-height: 40px;
-color: #E3E4E4;
+  /* position: absolute; */
+  width: 100%;
+  background: #232627;
+  /* height: 50px; */
+  padding: 10px 10px;
+  border: 1px solid #35373a;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 33px;
+  line-height: 40px;
+  color: #e3e4e4;
 }
 
 .widget_item {
-background: #232627;
-border: 1px solid #35373A;
-border-radius: 18px;
-    background: #232627;
-    border-radius: 20px;
-    padding: 0 10px 10px 10px;
+  background: #232627;
+  border: 1px solid #35373a;
+  border-radius: 18px;
+  background: #232627;
+  border-radius: 20px;
+  padding: 0 10px 10px 10px;
 }
 h2 {
-    /* Scoreboard */
+  /* Scoreboard */
 
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 36px;
 
-
-font-family: Inter;
-font-style: normal;
-font-weight: 600;
-font-size: 30px;
-line-height: 36px;
-
-color: #E3E4E4;
-
-
+  color: #e3e4e4;
 }
 .widgets {
-    overflow-x: hidden;
-    /* height: 100%; */
-    padding: 2%;
-      display: grid;
-      /* grid-template-columns: 10fr 1fr 1fr; */
-      gap: 2%;
-      justify-items: start;
-      align-items: start;
-        grid-template-areas: "a a a b" 
-                             "a a a c"
-                        
-;
+  overflow-x: hidden;
+  /* height: 100%; */
+  padding: 2%;
+  display: grid;
+  /* grid-template-columns: 10fr 1fr 1fr; */
+  gap: 2%;
+  justify-items: start;
+  align-items: start;
+  grid-template-areas:
+    "a a a b"
+    "a a a c";
 }
 @media screen and (min-width: 150px) {
-.widgets {
+  .widgets {
     display: flex;
     flex-direction: column;
-}
-.scoreboard  {
+  }
+  .scoreboard {
     min-width: 100% !important;
-}
+  }
 }
 .scoreboard {
-    min-width: 40vw;
-    grid-area: a;
+  min-width: 40vw;
+  grid-area: a;
 }
 .event-list {
-grid-area: b;
+  grid-area: b;
 }
 .future-event-list {
-grid-area: c;
+  grid-area: c;
 }
-
 </style>
