073077fc1002a19667965b975a0a35cf8eb76606
Albert Zagibin <albert.zagibin@gmail.com>
2024-06-07 15:15:32 +1000

    fix cs8618 problem

--- backend/DataAccess/Models/Api/ActivityRequest.cs
+++ backend/DataAccess/Models/Api/ActivityRequest.cs
@@ -2,7 +2,7 @@ namespace DataAccess.Models;
 
 public record ActivityRequest
 {
-    public string Name {get; set;}
-    public DateTime DateTime {get; set;}
-    public string Link {get; set;}
+    public string Name { get; set; } = null!;
+    public DateTime DateTime { get; set; }
+    public string Link { get; set; } = null!;
 }
