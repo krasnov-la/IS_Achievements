d98c4a038506039f4f1bdccf8ee22a591f913558
Krasnov-la <Leonid.Krasnov.R255@gmail.com>
2024-06-07 22:59:01 +1000

    json config change

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,7 +7,10 @@ using Microsoft.IdentityModel.Tokens;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("super_secret.json", false);
+if (builder.Environment.IsDevelopment())
+    builder.Configuration.AddJsonFile("super_secret.json", false);
+else 
+    builder.Configuration.AddEnvironmentVariables();
 var config = builder.Configuration;
 
 builder.Services.AddAuthentication(x => 
@@ -18,10 +21,10 @@ builder.Services.AddAuthentication(x =>
 }).AddJwtBearer(x => 
 {
     x.TokenValidationParameters = new TokenValidationParameters {
-        ValidIssuer = config["JwtSettings:Issuer"],
-        ValidAudience = config["JwtSettings:Audience"],
+        ValidIssuer = config["JwtIssuer"],
+        ValidAudience = config["JwtAudience"],
         IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)
+            Encoding.UTF8.GetBytes(config["JwtKey"]!)
         ),
         ValidateIssuer = true,
         ValidateAudience = true,
@@ -35,7 +38,7 @@ builder.Services.AddAuthorization(x =>
         p.RequireClaim(PolicyData.AdminClaimName, "true");
     });
 });
-builder.Services.AddDbContext<AppDbContext>(p => p.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(p => p.UseNpgsql(config["DbConnection"]));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
