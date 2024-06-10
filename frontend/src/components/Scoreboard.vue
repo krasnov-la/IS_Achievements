e8e1f08b096966ca21d8b835e64ed71f7e409b16
Albert Zagibin <albert.zagibin@gmail.com>
2024-06-10 23:13:28 +1000

    set up API_URL and add data from api to scoreboard

--- frontend/src/components/Scoreboard.vue
+++ frontend/src/components/Scoreboard.vue
@@ -2,7 +2,7 @@
   <div class="widget_item scoreboard">
     <div class="scoreboard-header">
       <h2>Scoreboard</h2>
-      <div class="dots"/>
+      <div class="dots" />
     </div>
 
     <div class="table">
@@ -11,10 +11,14 @@
         <p class="name1">Имя / никнэйм</p>
         <p class="score1">Количество баллов</p>
       </div>
-      <div class="table-item" v-for="player in players" :key="player.id">
-        <p class="number">#{{ player.id }}</p>
+      <div
+        class="table-item"
+        v-for="player in props.scoreboardData"
+        :key="player.id"
+      >
+        <p class="number">#{{ player.place }}</p>
         <img class="profile-img" src="" alt="" />
-        <p class="name">{{ player.name }}</p>
+        <p class="name">{{ player.nick }}</p>
         <p class="score">{{ player.score.toLocaleString("ru") }} баллов</p>
       </div>
     </div>
@@ -22,36 +26,12 @@
 </template>
 <script setup>
 import { ref, createApp } from "vue";
-const sortIndex = ref(0);
-const players = ref([
-  { name: "John", score: 100245, id: 1234 },
-  { name: "Jane", score: 93998, id: 1235 },
-  { name: "Bob", score: 86277, id: 1236 },
-  { name: "John", score: 100245, id: 1234 },
-  { name: "Jane", score: 93998, id: 1235 },
-  { name: "Bob", score: 86277, id: 1236 },
-]);
-
-function sortPlayers() {
-  sortIndex.value++; // Переход к следующему полю для сортировки
-  let fields = ["name", "score"]; // Массив полей для сортировки
-  if (sortIndex.value >= fields.length) {
-    sortIndex.value = 0; // Сброс индекса, если все поля были пройдены
-  }
-  players.value.sort((a, b) => {
-    switch (fields[sortIndex.value]) {
-      case "name":
-        return String(a[fields[sortIndex.value]]).localeCompare(
-          String(b[fields[sortIndex.value]])
-        );
-      case "score":
-        return a[fields[sortIndex.value]] - b[fields[sortIndex.value]];
-      default:
-        return 0;
-    }
-  });
-  const sortedPlayers = computed(() => players.value.slice()); // Копируем массив для отображения
-}
+const props = defineProps({
+  scoreboardData: {
+    type: Array,
+    default: () => [],
+  },
+});
 </script>
 <style scoped>
 h2 {
