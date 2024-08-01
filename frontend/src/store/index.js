3d1a66a2c9e2054140321384e1bb47c14e412bab
Albert Zagibin <albert.zagibin@gmail.com>
2024-08-01 12:44:24 +1000

    working personal sidebar

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,6 +4,7 @@ import createPersistedState from 'vuex-plugin-persistedstate'
 export default createStore({
   plugins: [createPersistedState()],
   state: {
+    activePath: '/',
     text: 'Главная страница',
     authenticated: false,
     user: {
@@ -13,16 +14,20 @@ export default createStore({
     },
   },
   mutations: {
+    SET_ACTIVE_PATH: (state, newPath) => state.activePath = newPath,
     SET_TEXT: (state, newText) => state.text = newText,
     SET_AUTH: (state, auth) => state.authenticated = auth,
     SET_USER: (state, user) => state.user = user,
   },
   actions: {
+    updateActivePath: ({commit}, newPath) => commit('SET_ACTIVE_PATH', newPath),
     updateText: ({ commit }, newText) => commit('SET_TEXT', newText),
     setAuth: ({commit}, auth) => commit('SET_AUTH', auth),
     setUser: ({commit}, user) => commit('SET_USER', user),
     },
   getters: {
+    activePath: state => state.activePath,
+    text: state => state.text,
     isAuthenticated: (state) => state.authenticated,
     user: (state) => state.user,
   }
